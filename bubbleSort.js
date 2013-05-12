71889778bb5329852b4367244e0d62c7c8f93df3
Fathea <teeeya@gmail.com>
2013-05-12 19:57:58 +0100

    bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,9 +1,9 @@
 //Bubble sort in javascript
-var unsortedArray = [90,90,4,5,6,12,3,4,5,0,209987,0.2];
-var numberOfChangesMade=0;
+var unsortedArray = [0,90,90,4,5,6,12,3,4,5,0,209987,0.2];
+var numberOfChangesMade=1;
 var numOfChangesTotal =0;
+var changed = true;
 function bubbleSort(unsortedArray){
-		numberOfChangesMades(unsortedArray); //call it for the first time to set numOfChangesTotal and numberOfChangesMade
 		while(numberOfChangesMade!=0){ //While there were chaneges made, check again
 			numberOfChangesMade = numberOfChangesMades();
 		}
@@ -22,6 +22,7 @@ function bubbleSort(unsortedArray){
 						numberOfChangesMade=numberOfChangesMade;
 					}
 			}
+
 			return numberOfChangesMade;
 		}
 	return unsortedArray;
